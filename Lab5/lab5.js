46c84400fc59c9325ae81cb160b957cda14442d6
Abigail Martinez <martinezabigail4146@gmail.com>
2026-03-15 17:09:58 -0400

    Add initial HTML files and assets for various projects
    - Created products.html for Owala FreeSip bottles with styling and layout.
    - Added index.html for multimedia podcast project with audio presentation and transcript.
    - Initialized media directory for podcast audio files.
    - Developed Index.html and lab5.js for JavaScript introduction with user input prompt.
    - Added pet images and styles for a pet gallery project.
    - Created index.html for pet gallery with responsive design and image grid.
    - Established project/index.html for Coffee Culture website with comprehensive content and interactive features.

---
+++ Lab5/lab5.js
@@ -0,0 +1,12 @@
+// JavaScript
+let movie = prompt("Hi! What is your favorite movie?")
+console.log("Your answer is ", movie);
+
+console.log(8 * null);
+console.log("5" - 1);
+console.log("5" + 1);
+console.log("five" * 2);
+console.log(false == 0);
+console.log(null == undefined);
+console.log(null == 0);
+
